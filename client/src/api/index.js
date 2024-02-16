7472709e85a3cd90deeae0bd59b82785e34291f2
Hirthick Raj D K <hirthickraj@jmangroup.com>
2024-02-16 14:28:54 +0530

    fixing deployment access

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create();
+const API = axios.create({ baseURL: "" });
 //post
 export const postExpense = async (payload) => {
   await API.post("/expense", payload);
