27028b958dada2b6ddfea92e64b92979b2904afb
Hirthick Raj D K <hirthickraj@jmangroup.com>
2024-02-16 14:28:19 +0530

    Fixing link

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:4000/api/" });
+const API = axios.create();
 //post
 export const postExpense = async (payload) => {
   await API.post("/expense", payload);
