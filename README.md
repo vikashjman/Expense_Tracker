c099aa240719df68b7956bd3fb1f00d1106907c8
Hirthick Raj D K <hirthickraj@jmangroup.com>
2024-02-16 13:57:32 +0530

    Deployment Changes

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # expenseTrackerJman# expenseTracker
 # Expense_Tracker
+
+
+### Figma flow for React Component
+https://www.figma.com/file/d96wR9sf05scW0Y9cnWqQ9/Untitled?type=whiteboard&node-id=0-1
--- server/server.js
+++ server/server.js
@@ -1,19 +1,24 @@
 const express = require("express");
+const path = require("path");
 const cors = require("cors");
-const morgan = require("morgan")
+const morgan = require("morgan");
 const { connectDB } = require("./utils/db");
 const dotenv = require("dotenv");
 const router = require("./routes/route");
 const { errorHandler, notFound } = require("./middleware/error.middleware");
 dotenv.config();
 
-const app = express(); 
+const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(morgan("tiny"))
+app.use(express.static(path.join(__dirname, "public")));
+app.use(morgan("tiny"));
 
-app.use(router);
+app.use("/api", router);
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+});
 
 app.use(notFound);
 app.use(errorHandler);
@@ -22,6 +27,9 @@ const PORT = process.env.PORT || 4000;
 
 connectDB().then(() => {
   app.listen(PORT, () => {
-    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT} . Visit http://localhost:${PORT}`.green.bold);
+    console.log(
+      `Server running in ${process.env.NODE_ENV} mode on port ${PORT} . Visit http://localhost:${PORT}`
+        .green.bold
+    );
   });
 });
